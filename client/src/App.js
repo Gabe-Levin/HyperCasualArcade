4456c9c21556b0cb7e33507ce8d654958d484fbc
Gabe-Levin <glev2190@gmail.com>
2022-04-20 19:04:39 +0200

    init, workplace and added primary components

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import GamesWindow from "./Components/GamesWindow";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <GamesWindow />
+    </div>
+  );
+}
+
+export default App;
