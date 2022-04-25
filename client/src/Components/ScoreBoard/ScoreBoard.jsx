320af07e1ad726d2588b4db9b8b381c155c5a4e8
Gabe-Levin <glev2190@gmail.com>
2022-04-25 10:48:50 +0200

    feat, now only showing the top 10 scores

--- client/src/Components/ScoreBoard/ScoreBoard.jsx
+++ client/src/Components/ScoreBoard/ScoreBoard.jsx
@@ -3,9 +3,10 @@ import Score from "../Score/Score";
 import "./ScoreBoard.css";
 
 export default function HighScores({ highScores }) {
+  const topTen = highScores.slice(0, 10);
   let position = 0;
 
-  const showScores = highScores?.map((score) => {
+  const showScores = topTen?.map((score) => {
     position++;
     return <Score key={position} score={score} position={position} />;
   });
