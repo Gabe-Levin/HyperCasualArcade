01d75517d65b3abf0e47a91f013afc8fce698d63
Gabe-Levin <glev2190@gmail.com>
2022-04-26 21:25:28 +0200

    feat, adding rocket emoji

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
+      <img className="spaceship" src="./imgs/rocket.png"></img>
       <a
         className="FooterText"
         href="https://github.com/Gabe-Levin/CW_Solo_HyperCasualArcade"
