6bb077db470460ee4c7ee97e9dc4560247b928a3
Gabe <glev2190@gmail.com>
2022-05-26 12:34:56 +0200

    feat: add jsx to eslint, covert ScoreBoard to ts

--- client/src/Components/ScoreBoard/ScoreBoard.tsx
+++ client/src/Components/ScoreBoard/ScoreBoard.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Score from "../Score/Score";
 import "./ScoreBoard.css";
 
-export default function HighScores({ highScores }) {
+export default function HighScores({ highScores }: {highScores:number[]}) {
   const topTen = highScores?.slice(0, 10);
   let position = 0;
 
-  const showScores = topTen?.map((score) => {
+  const showScores = topTen?.map((score: number[]) => {
     position++;
     return <Score key={position} score={score} position={position} />;
   });
